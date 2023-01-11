069b49a1d765d0d21f9a0b4e37f1ec5a33b457df
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-01-11 17:59:01 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import Navbar from './components/Navbar'
+import Layout from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>COVIDERS</h1>
+        <Layout/>
       </header>
     </div>
-  );
-};
+  );    
+};      
 
 export default App;
