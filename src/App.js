2e93762bdd80887fe06b60f5899d8f1a6e9c9a0b
DianaB177 <anaidb77@gmail.com>
2023-01-12 17:58:29 +0100

    update dates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import './App.css';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Layout from './components/Navbar'
-=======
-import {Tracker_seis, data, options} from './components/Tracker_seis';
-=======
 import DataContainer from './components/DataContainer';
->>>>>>> 7ecee47a2b568124977686b50bfae83928cbe594
-=======
-import Layout from './components/moleculas/Navbar'
-import {Tracker_seis, data, options} from './components/Tracker_seis';
+import Drawer from './components/moleculas/Drawer';
+//import {Tracker_seis, data, options} from './components/Tracker_seis';
 import CasesComponent from './components/covid19tracker';
->>>>>>> 7a07e898d9b7737db7fd40647ff1fbf953e18374
 
 
 function App() {
@@ -20,21 +10,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>COVIDERS</h1>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <Layout/>
-=======
-        <Tracker_seis />
->>>>>>> fa8c588be21976b40fe47ac61126d280377d03f5
-=======
+        <Drawer/>
+        
         <DataContainer />
->>>>>>> 7ecee47a2b568124977686b50bfae83928cbe594
-=======
-        <Layout/>
-        <Tracker_seis/>
         <CasesComponent/>
->>>>>>> 7a07e898d9b7737db7fd40647ff1fbf953e18374
       </header>
     </div>
   );    
--- src/components/moleculas/Drawer.jsx
+++ src/components/moleculas/Drawer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Drawer from '@mui/material/Drawer';
+//import Drawer from '@mui/material/Drawer';
 import { flexbox } from '@mui/system';
 import List from '@mui/material/List'; //ul
 import ListItem from '@mui/material/ListItem'; //li
@@ -58,7 +58,7 @@ const menuItems = [
     },
 ]
 
-export default function Layout({children}) {
+export default function Drawer({children}) {
     const classes = {
         drawer: {
           width: drawerWidth,
