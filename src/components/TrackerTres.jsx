98d709a4149750477e6d1ad2b10f1a26d5070551
DianaB177 <anaidb77@gmail.com>
2023-01-12 09:04:01 +0100

    porps changes

--- src/components/TrackerTres.jsx
+++ src/components/TrackerTres.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const TrackerTres = ({}) => {
   <>
   <h2>{country}</h2>
-  <img src={bags} alt={country}></img>
+  <img src={bag} alt={country}></img>
   </>
  }
     TrackerTres.propTypes = {
