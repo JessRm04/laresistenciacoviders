53cfc55fa803a4396eae00863d56137357f6406b
natalia <natiriarte5@gmail.com>
2023-01-13 14:47:25 +0100

    funcional

--- src/components/Desplegable.jsx
+++ src/components/Desplegable.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import '../css/trackerdos.css';
+import { useState, useEffect } from 'react';
 
 function desplegable() {
+  const [data, setData] = useState([]);
+    
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('https://disease.sh/v3/covid-19/countries');
+            const json = await response.json();
+            setData(json);
+        }
+        fetchData();
+        },[]);
+        
   return (
+    <div>
+       {data && (
     <select id="my-select">
-  <option value="option1">Option 1</option>
-  <option value="option2">Option 2</option>
-  <option value="option3">Option 3</option>
+  <option value="option1">{data.updated}</option>
 </select>
-
+)}
+</div>
   )
 }
 
--- src/components/Trackerdos.jsx
+++ src/components/Trackerdos.jsx
@@ -1,33 +1,48 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import covidred from '../assets/img/covidred.svg';
 import covidblue from '../assets/img/covidblue.svg';
 import covidorange from '../assets/img/covidorange.svg';
 import covidgreen from '../assets/img/covidgreen.svg';
 import telephone from '../assets/img/telephone.svg';
 import '../css/trackerdos.css';
+import Desplegable from './Desplegable';
 
 
 const Trackerdos = () => {
-  return (
-<body> 
-<div>
+    const [data, setData] = useState([]);
+    
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('https://disease.sh/v3/covid-19/all');
+            const json = await response.json();
+            setData(json);
+        }
+        fetchData();
+        },[]);
+        
+    return (
+<>
+<Desplegable />
+    {data && (
+<div className='track2'>
     <div className='container-colum'>
     <div className='div1'>
         <div className='casos'>
             <h5>Total Cases</h5>
             <hr />
-            <h2>58711977</h2>
+            <h2>{data.cases}</h2>
         </div>
         <div className='virus-v'>
             <img className="virus" src={covidblue} alt="virus" width="100px" />
         </div>
     </div>
-
+        
     <div className='div2'>
         <div className='casos'>
             <h5>Total Deaths</h5>
             <hr />
-            <h2>58711977</h2>
+            <h2>{data.deaths}</h2>
         </div>
         <div className='virus-v'>
             <img className="virus" src={covidred} alt="virus" width="100px" />
@@ -37,7 +52,7 @@ const Trackerdos = () => {
         <div className='casos'>
             <h5>Total Recovered</h5>
             <hr />
-            <h2>58711977</h2>
+            <h2>{data.recovered}</h2>
         </div>
         <div className='virus-v'>
             <img className="virus" src={covidgreen} alt="virus" width="100px" />
@@ -49,7 +64,7 @@ const Trackerdos = () => {
         <div className='casos'>
             <h5>Total Active</h5>
             <hr />
-            <h2>58711977</h2>
+            <h2>{data.active}</h2>
         </div>
         <div className='virus-v'>
             <img className="virus" src={covidblue} alt="virus" width="100px" />
@@ -60,7 +75,7 @@ const Trackerdos = () => {
         <div className='casos'>
             <h5>New Cases</h5>
             <hr />
-            <h2>0</h2>
+            <h2>{data.todayCases}</h2>
         </div>
         <div className='virus-v'>
             <img className="virus" src={covidorange} alt="virus" width="100px" />
@@ -71,7 +86,7 @@ const Trackerdos = () => {
         <div className='casos'>
             <h5>New Deaths</h5>
             <hr />
-            <h2>0</h2>
+            <h2>{0}</h2>
         </div>
         <div className='virus-v'>
             <img className="virus" src={covidred} alt="virus" width="100px" />
@@ -85,7 +100,7 @@ const Trackerdos = () => {
         </div>
         <div className='confirmed'>
             <h6>Total Confirmed</h6>
-            <h3>374111576</h3>
+            <h3>{data.cases}</h3>
         </div>
     </div>
 
@@ -95,7 +110,7 @@ const Trackerdos = () => {
         </div>
         <div className='confirmed'>
             <h6>Total Recovered</h6>
-            <h3>374111576</h3>
+            <h3>{data.recovered}</h3>
         </div>
     </div>
     
@@ -105,7 +120,7 @@ const Trackerdos = () => {
         </div>
         <div className='confirmed'>
             <h6>Total Deadths</h6>
-            <h3>374111576</h3>
+            <h3>{data.deaths}</h3>
         </div>
     </div>
     
@@ -115,7 +130,7 @@ const Trackerdos = () => {
         </div>
         <div className='confirmed'>
             <h6>New Deadths</h6>
-            <h3>374111576</h3>
+            <h3>{0}</h3>
         </div>
     </div>
     
@@ -129,9 +144,17 @@ const Trackerdos = () => {
         </div>
     </div>
     </div>
+    
 </div>
-</body> 
+)}
+</> 
+
   );
 }
 
 export default Trackerdos;
+
+
+
+
+
