704117ee42b09a75d85934b861337cba4c0dcf0e
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-01-14 17:37:00 +0100

    starting top10 again

--- src/components/covid19tracker.jsx
+++ src/components/covid19tracker.jsx
@@ -4,22 +4,6 @@ import Recuadro from './atomos/RecuadroTracker';
 // import ApiData from './atomos/Tracker19function';
 
 
-// const api_totals = <ApiData/>  
-// const api_active = "API números"
-// const api_recovered = "API números"
-// const api_deaths = "API números"
-
-// function CasesComponent ({texto, numeros}){
-//     return(
-//         <div className="containerStyle">
-//             <Recuadro texto="Total Case" numeros={api_totals} clase="recuadroStyle" />
-//             <Recuadro texto="Active Case" numeros={api_active} clase="recuadroStyle2"/>
-//             <Recuadro texto="Recovered Case" numeros={api_recovered} clase="recuadroStyle3"/>
-//             <Recuadro texto="Deaths Case" numeros={api_deaths} clase="recuadroStyle4"/>
-//         </div>
-//         )}
-
-// export default CasesComponent;
 
 function CasesComponent() {
     const [data, setData] = useState(null);
--- src/components/moleculas/top10.jsx
+++ src/components/moleculas/top10.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Top10Component from "../atomos/Top10Component.jsx"
-import coronavirus from '../../img/coronavirus.png'
 
 
 
-function Top10 ({src, country, numbers}){
-    
-    const ciudad = "country"
-    const number = "0000"
-    const co = {coronavirus}
-    return(
+function Top10() {
+     const [data, setData] = useState(null);
+   
+     useEffect(() => {
+       async function fetchData() {
+         const response = await fetch(`https://disease.sh/v3/covid-19/all`);
+         const json = await response.json();
+         setData(json);
+       }
+       fetchData();
+     });
+   
+     return (
        <div>
-            <Top10Component src={co} country={ciudad} numbers={number}/>
+         {data && (
+          <div>
+          <Top10Component src={'hola'} country={data.cases} numbers={data.cases}/>
+         </div>
+          )}
        </div>
-        )}
-
-export default Top10;
+     );
+   }
+   
+   export default Top10;
