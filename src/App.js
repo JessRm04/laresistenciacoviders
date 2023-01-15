4218d0d93d7dedd143e9268b8fefe91bf82992d0
DianaB177 <anaidb77@gmail.com>
2023-01-15 23:14:09 +0100

    including map training

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React, { memo } from 'react';
 import './App.css';
 //import Drawer from './components/moleculas/Mol-drawer';
 import CasesComponent from './components/covid19tracker';
 import TrackerCuatro from './components/tracker-cuatro/Tracker-cuatro';
- 
+import TrackerSiete from './components/tacker-siete/Tracker-siete';
+import MapChart from './components/tacker-siete/Tracker-siete-bis';
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         {/* <Drawer/> */}
         <CasesComponent/> 
         <TrackerCuatro />
+        <TrackerSiete />
+        <MapChart/>
       </header>
     </div>
   );    
---
+++ src/components/tacker-siete/Tracker-siete-class.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import WorldMap from "react-svg-worldmap";
+
+export default class App extends Component {
+  render() {
+    // ...
+    const data = [
+      { country: "cn", value: 1389618778 }, // china
+      { country: "in", value: 1311559204 }, // india
+    ];
+    // ...
+    return (
+      <div id="root">
+        <WorldMap color="green" title="This is My Map" size="lg" data={data} />
+      </div>
+    );
+  }
+}
