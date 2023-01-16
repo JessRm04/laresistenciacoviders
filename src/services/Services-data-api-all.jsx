b4f5ace3485defd79b2a73e6229ffa4d270f1a46
DianaB177 <anaidb77@gmail.com>
2023-01-16 16:44:40 +0100

    update-api-data-all

--- src/services/Services-data-api-all.jsx
+++ src/services/Services-data-api-all.jsx
@@ -4,7 +4,12 @@ const DataApiAll = () => {
     const [allData, setAllData] = useState([]);
     useEffect(() => {
         async function fetchData() {
-          const response = await fetch("https://disease.sh/v3/covid-19/all");
+          const config = {
+            headers: {
+              Accept: "application/json",
+            },
+          };
+          const response = await fetch("https://disease.sh/v3/covid-19/all", config);
           const responseJson = await response.json();
           setAllData(responseJson); 
         }
