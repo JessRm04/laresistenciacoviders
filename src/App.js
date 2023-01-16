e7d2f15d446052213926b8f951120ce6bbfb5730
DianaB177 <anaidb77@gmail.com>
2023-01-16 20:27:14 +0100

    everythings ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import './App.css';
-import Layout from './components/Layout';
-import PreventLanding from './components/moleculas/Mol-Landing-prevent';
+// import Layout from './components/Layout';
+// import PreventLanding from './components/moleculas/Mol-Landing-prevent';
 // import Top10 from './components/moleculas/Mol-top10';
-//import CasesComponent from './components/covid19tracker'
-// import Trackerdos from './components/render/trackers/tracker-dos/Mol-trackerdos';
-// import Desplegable from './components/render/trackers/tracker-dos/Mol-trackerdos-desplegable';
-// import Layout from './components/moleculas/Mol-layout';
 // import CasesComponent from './components/covid19tracker';
+// import Desplegable from './components/render/trackers/tracker-dos/Mol-trackerdos-desplegable';
+// //import Layout from './components/moleculas/Mol-layout';
 // import TrackerCuatro from './components/render/trackers/tracker-cuatro/Tracker-cuatro';
+import Trackerdos from './components/render/trackers/tracker-dos/Mol-trackerdos';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Layout/>
-        {/* <Top10/> */}
+        {/* <Layout/>
+        <Top10/>
         <PreventLanding/>
-        {/* <Desplegable />
+        <Desplegable /> */}
         <Trackerdos />
-        <CasesComponent/> 
+        {/* <CasesComponent/> 
         <TrackerCuatro /> */}
         <covid19tracker/>
       </header>
