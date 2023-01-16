35f7ee0b7be2baafc369e0da0b37093a4b74012e
SharonInfante <sharonshabel@gmail.com>
2023-01-16 20:45:20 +0100

    landing question lista

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './App.css';
-import Trackerdos from './components/render/trackers/tracker-dos/Mol-trackerdos';
-import Desplegable from './components/render/trackers/tracker-dos/Mol-trackerdos-desplegable';
+//import Trackerdos from './components/render/trackers/tracker-dos/Mol-trackerdos';
+//import Desplegable from './components/render/trackers/tracker-dos/Mol-trackerdos-desplegable';
 //import Layout from './components/moleculas/Mol-layout';
-import CasesComponent from './components/covid19tracker';
-import TrackerCuatro from './components/render/trackers/tracker-cuatro/Tracker-cuatro';
+//import CasesComponent from './components/covid19tracker';
+//import TrackerCuatro from './components/render/trackers/tracker-cuatro/Tracker-cuatro';
+import Question from './components/render/landing/landing-view-questions/landing-view-question';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>COVIDERS</h1>
-        {/* <Drawer/> */}
+        {/* <h1>COVIDERS</h1>
+        <Drawer/> 
         <Desplegable />
         <Trackerdos />
         <CasesComponent/> 
-        <TrackerCuatro />
+        <TrackerCuatro />*/}
+        <Question/>
       </header>
     </div>
   );    
