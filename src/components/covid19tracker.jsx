9b682c943345bff1987fac8c29a20e40558d11bf
DianaB177 <anaidb77@gmail.com>
2023-01-16 18:50:59 +0100

    update

--- src/components/covid19tracker.jsx
+++ src/components/covid19tracker.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
-//import '../atomos/recuadroTrackerStyle.css';
 import Recuadro from './atomos/RecuadroTracker';
-import DataApiAll from '../services/Services-data-api-all';
+import DataApiAll from '../services/Services-data-api-all'
 
 
 
 const CasesComponent = ()=>{
     const responseData = DataApiAll();
     return(
-       <div className="containerStyle" key={responseData.countryInfo._id}>
-            <Recuadro texto="Total Case" numeros={responseData.totals}/>
+       <div className="containerStyle">
+            <Recuadro texto="Total Case" numeros={responseData.todayCases}/>
             <Recuadro texto="Active Case" numeros={responseData.active}/>
             <Recuadro texto="Recovered Case" numeros={responseData.recovered}/>
-            <Recuadro texto="Deaths Case" numeros={responseData.deaths}/>
+            <Recuadro texto="Deaths Case" numeros={responseData.todayDeaths}/>
         </div> 
    );
 }
