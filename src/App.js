35e90f32da0a67321a2a2f50310c6f196843d6e6
DianaB177 <anaidb77@gmail.com>
2023-01-17 14:24:27 +0100

    match-landing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Layout from './components/moleculas/Mol-layout';
+import Layout from './components/render/trackers/tracker-layout/Layout';
 import TrackerCuatro from './components/render/trackers/tracker-cuatro/Tracker-cuatro';
 import Trackerdos from './components/render/trackers/tracker-dos/Mol-trackerdos';
 import Desplegable from './components/render/trackers/tracker-dos/Mol-trackerdos-desplegable';
