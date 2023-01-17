7bd29456f0a3b9f65228523e24cc890e031f5a30
Diana Borrajo <90630004+Dianab177@users.noreply.github.com>
2023-01-17 14:48:22 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
-import React from 'react';
-import './App.css';
-import Layout from './components/render/trackers/tracker-layout/Layout';
-import TrackerCuatro from './components/render/trackers/tracker-cuatro/Tracker-cuatro';
-import Trackerdos from './components/render/trackers/tracker-dos/Mol-trackerdos';
-import Desplegable from './components/render/trackers/tracker-dos/Mol-trackerdos-desplegable';
-import Question from './components/render/landing/landing-view-questions/landing-view-question';
-import Washed from './components/render/landing/landing-view-wash/landing-view-wash';
-import PreventLanding from './components/moleculas/Mol-Landing-prevent';
-import Top10 from './components/moleculas/Mol-top10';
-import CasesComponent from './components/moleculas/Mol-tracker-covid19/covid19tracker'
+import React from "react";
+import "./App.css";
+import Layout from "./components/render/trackers/tracker-layout/Layout";
+import TrackerCuatro from "./components/render/trackers/tracker-cuatro/Tracker-cuatro";
+import Trackerdos from "./components/render/trackers/tracker-dos/Mol-trackerdos";
+import Desplegable from "./components/render/trackers/tracker-dos/Mol-trackerdos-desplegable";
+import Question from "./components/render/landing/landing-view-questions/landing-view-question";
+import Washed from "./components/render/landing/landing-view-wash/landing-view-wash";
+import PreventLanding from "./components/moleculas/Mol-Landing-prevent";
 import TrackerDiecinueve from "./components/render/trackers/tracker-covid19/Tracker-covid-diecinueve";
-import News from './components/render/landing/landing-news/News';
+import News from "./components/render/landing/landing-news/News";
 import Symptom from "./components/render/landing/landing-symptom/Symptom";
 import Spreads from "./components/render/landing/landing-spreads/Spreads";
-import Footer from './components/render/landing/footer/Footer';
+import Footer from "./components/render/landing/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>COVIDERS</h1>
-        <Symptom/>
-        <PreventLanding/>
-        <Spreads/>
+        <Symptom />
+        <PreventLanding />
+        <Spreads />
         <Question />
         <Washed />
         <News />
-        <Footer/>
+        <Footer />
         <Desplegable />
-        <CasesComponent />
         <Layout />
         <Trackerdos />
         <TrackerCuatro />
-        <TrackerDiecinueve/>
-        <Top10 />
-        
-        
-        
+        <TrackerDiecinueve />
       </header>
     </div>
   );
