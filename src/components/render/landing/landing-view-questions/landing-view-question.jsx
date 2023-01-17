4d39c08ba8c02eae3dbccc27d94a18bfda1004fa
SharonInfante <sharonshabel@gmail.com>
2023-01-17 12:31:31 +0100

    modificado estilos

--- src/components/render/landing/landing-view-questions/landing-view-question.jsx
+++ src/components/render/landing/landing-view-questions/landing-view-question.jsx
@@ -10,28 +10,28 @@ function Question() {
         </div>
         <div className='questionCards'>
             <div className='questionCardsLeft'>
-                <div className='question2 questionOrder01'>
+                <div className='question2'>
                     <h4>01. How does COVID-19 spread?</h4>
                     <p>Data has shown that it spreads from person to person among those in close contact 
                     (within about 6 feet, or 2 meters). The virus spreads by respiratory droplets
                     released when someone infected with the virus.
                     </p>
                 </div>
-                <div className='question questionOrder02'>
+                <div className='question'>
                     <h4>2. What are the symptoms of COVID-19?</h4>
                 </div>
-                <div className='question questionOrder03'>
+                <div className='question'>
                     <h4>3. Should I wear mask?</h4>
                 </div>
             </div>
             <div className='questionCardsRight'>
-                <div className='question questionOrder04'>
+                <div className='question'>
                     <h4>04. What does it mean to self-isolate?</h4>
                 </div>
-                <div className='question questionOrder05'>
+                <div className='question'>
                     <h4>05. Can children or adolescents catch COVID-19?</h4>
                 </div>
-                <div className='question2 questionOrder06'>
+                <div className='question2'>
                     <h4>06. Is there a vaccine or drug for COVID-19?</h4>
                     <p>While some western, traditional or home remedies may provide comfort and alleviate symptoms
                     of mild COVID-19, there are no medicines that have been shown to prevent or cure the disease. 
