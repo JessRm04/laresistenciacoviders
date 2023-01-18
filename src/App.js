43743e1c0d7e0519fe28ad8e227449223f220fc7
DianaB177 <anaidb77@gmail.com>
2023-01-18 12:21:15 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
 // import Layout from "./components/render/trackers/tracker-layout/Layout";
-//import TrackerCuatro from "./components/render/trackers/tracker-cuatro/Tracker-cuatro";
+// import TrackerCuatro from "./components/render/trackers/tracker-cuatro/Tracker-cuatro";
 // import Trackerdos from "./components/render/trackers/tracker-dos/Tracker-dos";
 // import Question from "./components/render/landing/landing-view-questions/landing-view-question";
 // import Washed from "./components/render/landing/landing-view-wash/landing-view-wash";
 // import PreventLanding from "./components/moleculas/Mol-Landing-prevent";
-// import TrackerDiecinueve from "./components/render/trackers/tracker-covid19/Tracker-covid-diecinueve";
+ import TrackerDiecinueve from "./components/render/trackers/tracker-covid19/Tracker-covid-diecinueve";
 // import News from "./components/render/landing/landing-news/News";
 // import Symptom from "./components/render/landing/landing-symptom/Symptom";
 // import Spreads from "./components/render/landing/landing-spreads/Spreads";
 // import Footer from "./components/render/landing/footer/Footer";
-//import TrackerDiecinueveRight from './components/render/trackers/tracker-diecinueve-right/Tracker-diecinueve-right';
-//import MolTrackerDiecinueveFlags from "./components/render/trackers/tracker-diecinueve-right/Mol-tracker-diecinueve-flag";
-import MolTrackerDiecinuevecases from "./components/render/trackers/tracker-diecinueve-right/Mol-tracker-diecinueve-cases";
+ //import TrackerDiecinueveRight from './components/render/trackers/tracker-diecinueve-right/Tracker-diecinueve-right';
+// import MolTrackerDiecinueveFlags from "./components/render/trackers/tracker-diecinueve-right/Mol-tracker-diecinueve-flag";
+// import MolTrackerDiecinuevecases from "./components/render/trackers/tracker-diecinueve-right/Mol-tracker-diecinueve-cases";
 function App() {
   return (
     <div className="App">
@@ -25,14 +25,14 @@ function App() {
         <Question />
         <Washed />
         <News />
-        <Footer />
-        <Layout /> */}
-        {/* <Trackerdos />
-        <TrackerCuatro />
-        <TrackerDiecinueve /> */}
+        <Footer /> */}
+        {/* <Layout /> */}
+        {/* <Trackerdos /> */}
+        {/* <TrackerCuatro /> */}
+        {/* <TrackerDiecinueve /> */}
         {/* <TrackerDiecinueveRight/> */}
         {/* <MolTrackerDiecinueveFlags /> */}
-        <MolTrackerDiecinuevecases/>
+        {/* <MolTrackerDiecinuevecases/> */}
       </header>
     </div>
   );
