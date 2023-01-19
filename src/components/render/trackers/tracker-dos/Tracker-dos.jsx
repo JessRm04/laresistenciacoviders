77f3cf57638646fe9cabf25ab83722e885011e24
DianaB177 <anaidb77@gmail.com>
2023-01-19 20:28:27 +0100

    css tracker-dos

--- src/components/render/trackers/tracker-dos/Tracker-dos.jsx
+++ src/components/render/trackers/tracker-dos/Tracker-dos.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Desplegable from './Mol-tracker-dos-desplegable';
 import MolTrackerDosCountry from './Mol-tracker-dos-country';
-import MolTrackerDosWorld from './Mol-tracker-dos-world'; 
-
+import MolTrackerDosWorld from './Mol-tracker-dos-world';
+import './tracker-dos.css'
 
 
 const TrackerDos = () => {
--- src/components/render/trackers/tracker-raiz/Tracker-raiz.jsx
+++ src/components/render/trackers/tracker-raiz/Tracker-raiz.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TrackerDiecinueve from '../tracker-covid19/Tracker-covid-diecinueve';
 import TrackerCuatro from '../tracker-cuatro/Tracker-cuatro';
-// import Trackerdos from '../tracker-dos/Mol-trackerdos';
+import Trackerdos from '../tracker-dos/Tracker-dos';
 import Layout from '../tracker-layout/Layout';
 import "./tracker-raiz.css"
 import TrackerSeis from "../tracker-seis/Tracker_seis.jsx"
-import Trackerdos from '../tracker-dos/Mol-trackerdos';
 import TableTracker from '../../../moleculas/mol-track-tres-table/Mol-track-tres-table';
 
 const TrackerRaiz = () => {
