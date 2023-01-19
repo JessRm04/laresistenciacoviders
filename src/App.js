f6b739d40df203c0cce1799a506a30c59813b663
DianaB177 <anaidb77@gmail.com>
2023-01-19 11:26:19 +0100

    order

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-//import Layout from "./components/render/trackers/tracker-layout/Layout";
 import TrackerCuatro from "./components/render/trackers/tracker-cuatro/Tracker-cuatro";
 import Trackerdos from "./components/render/trackers/tracker-dos/Mol-trackerdos";
 import Question from "./components/render/landing/landing-view-questions/landing-view-question";
@@ -11,26 +10,10 @@ import News from "./components/render/landing/landing-news/News";
 import Symptom from "./components/render/landing/landing-symptom/Symptom";
 import Spreads from "./components/render/landing/landing-spreads/Spreads";
 import Footer from "./components/render/landing/footer/Footer";
-//import Buttons from "./components/atomos/buttons/Buttons.jsx";
 import LandingMobile from "./components/render/landing/landing-mobile/Landing-mobile";
 //import TrackerRaiz from "./components/render/trackers/tracker-raiz/Tracker-raiz";
 import TableTracker from './components/moleculas/mol-track-tres-table/Mol-track-tres-table';
 import DataApi from './services/Services-data-api-countries';
-
-// import Layout from "./components/render/trackers/tracker-layout/Layout";
-// import TrackerCuatro from "./components/render/trackers/tracker-cuatro/Tracker-cuatro";
-// import Trackerdos from "./components/render/trackers/tracker-dos/Mol-trackerdos";
-// import Desplegable from "./components/render/trackers/tracker-dos/Mol-trackerdos-desplegable";
-// import Question from "./components/render/landing/landing-view-questions/landing-view-question";
-// import Washed from "./components/render/landing/landing-view-wash/landing-view-wash";
-// import PreventLanding from "./components/moleculas/Mol-Landing-prevent";
-// import TrackerDiecinueve from "./components/render/trackers/tracker-covid19/Tracker-covid-diecinueve";
-// import News from "./components/render/landing/landing-news/News";
-// import Symptom from "./components/render/landing/landing-symptom/Symptom";
-// import Spreads from "./components/render/landing/landing-spreads/Spreads";
-// import Footer from "./components/render/landing/footer/Footer";
-// import Buttons from "./components/atomos/buttons/Buttons.jsx";
-// import LandingMobile from "./components/render/landing/landing-mobile/Landing-mobile";
 import TrackerRaiz from "./components/render/trackers/tracker-raiz/Tracker-raiz";
 
 function App() {
@@ -42,30 +25,20 @@ function App() {
         <h1>COVIDERS</h1>
         <Symptom />
         <PreventLanding />
-
-        <TrackerRaiz />
-        {/*<Symptom />
-        <Buttons/>
+        
+        <Symptom />
         <PreventLanding/>
-
         <Spreads />
         <Question />
         <Washed />
         <News />
         <LandingMobile/>
         <Footer />
-        {/* <Layout /> */}
+        <TrackerRaiz />
         <Trackerdos />
         <TableTracker data= {DataApi()}/>
         <TrackerCuatro />
-
-        <TrackerDiecinueve/>
-        <Layout />        
-
-  <TrackerDiecinueve/>*/}
-   {/* <Layout /> */}
-
-
+        <TrackerDiecinueve/>       
       </header>
     </div>
   );
