5d38b530f99c8b2019fcc02cd5a8a9deb9684835
SharonInfante <sharonshabel@gmail.com>
2023-01-22 14:51:53 +0100

    sin resultado

--- src/components/render/trackers/tracker-cinco/Mol-tracker-cinco-cases.jsx
+++ src/components/render/trackers/tracker-cinco/Mol-tracker-cinco-cases.jsx
@@ -11,84 +11,86 @@ import coviddark from "../../../../assets/img/covid-redark.svg";
 import covidefult from "../../../../assets/img/covid-defult.svg";
 
 const MolTrackerCincoCases = () => {
-    const responseData = DataApi();
+
+  const responseData = DataApi();
+
   return responseData
-        .sort((a, b) => b.cases - a.cases)
-        .slice(0, 1)
-        .map((item) =>(
-            <div id={item.country} data-country-code={item.country} key={item.countryInfo._id}>
-            <div className="covid-world-widget">
-              <div className="updated-time row">
-                <div className="col-md-4 col-lg-3 col-6">
-                  <div className="covid-widget-1 defult-bx">
-                    <div className="icon totalCase">
-                      <img src={covidefult} alt=""/>
-                    </div>
-                    <div className="info">
-                      <h5>Total Cases</h5>
-                      <h3>{item.todayCases}</h3>
-                    </div>
-                  </div>
+    .sort((a, b) => b.cases - a.cases)
+    .slice(0, 1)
+    .map((item) =>(
+      <div id={item.country} data-country-code={item.country} key={item.countryInfo._id}>
+        <div className="covid-world-widget">
+          <div className="updated-time row">
+            <div className="col-md-4 col-lg-3 col-6">
+              <div className="covid-widget-1 defult-bx">
+                <div className="icon totalCase">
+                  <img src={covidefult} alt=""/>
                 </div>
-                <div className="col-md-4 col-lg-3 col-6">
-                  <div className="covid-widget-1 red-bx">
-                    <div className="icon">
-                      <img src={covidred} alt="" />
-                    </div>
-                    <div className="info totalDeaths">
-                      <h5>Total Deaths</h5>
-                      <h3>{item.deaths}</h3>
-                    </div>
-                  </div>
+                <div className="info">
+                  <h5>Total Cases</h5>
+                  <h3>{item.todayCases}</h3>
                 </div>
-                <div className="col-md-4 col-lg-3 col-6">
-                  <div className="covid-widget-1 green-bx">
-                    <div className="icon">
-                      <img src={covidgreen} alt="" />
-                    </div>
-                    <div className="info totalRecovered">
-                      <h5>Total Recovered</h5>
-                      <h3>{item.todayRecovered}</h3>
-                    </div>
-                  </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-lg-3 col-6">
+              <div className="covid-widget-1 red-bx">
+                <div className="icon">
+                  <img src={covidred} alt="" />
                 </div>
-                <div className="col-md-4 col-lg-3 col-6">
-                  <div className="covid-widget-1 blue-bx">
-                    <div className="icon">
-                      <img src={covidblue} alt="" />
-                    </div>
-                    <div className="info totalActive">
-                      <h5>Total Active</h5>
-                      <h3>{item.active}</h3>
-                    </div>
-                  </div>
+                <div className="info totalDeaths">
+                  <h5>Total Deaths</h5>
+                  <h3>{item.deaths}</h3>
                 </div>
-                <div className="col-md-4 col-lg-3 col-6">
-                  <div className="covid-widget-1 orange-bx">
-                    <div className="icon">
-                      <img src={covidorange} alt="" />
-                    </div>
-                    <div className="info newCases">
-                      <h5>New Cases</h5>
-                      <h3>{item.todayCases}</h3>
-                    </div>
-                  </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-lg-3 col-6">
+              <div className="covid-widget-1 green-bx">
+                <div className="icon">
+                  <img src={covidgreen} alt="" />
                 </div>
-                <div className="col-md-4 col-lg-3 col-6">
-                  <div className="covid-widget-1 redark-bx">
-                    <div className="icon">
-                      <img src={coviddark} alt="" />
-                    </div>
-                    <div className="info newDeaths">
-                      <h5>New Deaths</h5>
-                      <h3>{item.todayDeaths}</h3>
-                    </div>
-                  </div>
+                <div className="info totalRecovered">
+                  <h5>Total Recovered</h5>
+                  <h3>{item.todayRecovered}</h3>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-lg-3 col-6">
+              <div className="covid-widget-1 blue-bx">
+                <div className="icon">
+                  <img src={covidblue} alt="" />
+                </div>
+                <div className="info totalActive">
+                  <h5>Total Active</h5>
+                  <h3>{item.active}</h3>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-lg-3 col-6">
+              <div className="covid-widget-1 orange-bx">
+                <div className="icon">
+                  <img src={covidorange} alt="" />
+                </div>
+                <div className="info newCases">
+                  <h5>New Cases</h5>
+                  <h3>{item.todayCases}</h3>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-lg-3 col-6">
+              <div className="covid-widget-1 redark-bx">
+                <div className="icon">
+                  <img src={coviddark} alt="" />
+                </div>
+                <div className="info newDeaths">
+                  <h5>New Deaths</h5>
+                  <h3>{item.todayDeaths}</h3>
                 </div>
               </div>
             </div>
           </div>
-        )
+        </div>
+      </div>
+    )
   );
 }
 
--- src/components/render/trackers/tracker-cinco/Mol-tracker-cinco-flag.jsx
+++ src/components/render/trackers/tracker-cinco/Mol-tracker-cinco-flag.jsx
@@ -6,20 +6,20 @@ import "bootstrap/dist/css/bootstrap.css";
 const MolTrackerCincoFlags = () => {
   const responseData = DataApi();
   return responseData
-        .sort((a, b) => b.cases - a.cases)
-        .slice(0, 9)
-        .map((item) =>(
-        <div className="nav-item" key={item.countryInfo._id}>
-              <a data-toggle="tab" href={item.country} >
-                <img
-                  alt={item.country}
-                  src={item.countryInfo.flag}
-                  width="30"
-                />
-                {item.country}
-              </a>
-        </div>
-        )
+    .sort((a, b) => b.cases - a.cases)
+    .slice(0, 9)
+    .map((item) =>(
+      <div className="nav-item" key={item.countryInfo._id}>
+        <a data-toggle="tab" href={item.country} >
+          <img
+            alt={item.country}
+            src={item.countryInfo.flag}
+            width="30"
+          />
+          {item.country}
+        </a>
+      </div>
+    )
   );
 }
 
