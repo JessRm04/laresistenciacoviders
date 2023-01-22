c64076a60c7f936cb21b8ad536dbe586f0816f8e
DianaB177 <anaidb77@gmail.com>
2023-01-22 15:23:19 +0100

    update

--- src/components/render/trackers/tracker-dos/Mol-tracker-dos-country.jsx
+++ src/components/render/trackers/tracker-dos/Mol-tracker-dos-country.jsx
@@ -11,113 +11,115 @@ const MolTrackerDosCountry = () => {
   const responseData = DataApiAll();
 
   return (
-    <div className="container-country">
+    <>
       {responseData && (
-        <div className="track2">
-          <div className="container-colum">
-            <div className="div1">
-              <div className="casos">
-                <h5>Total Cases</h5>
-                <div className="linea">
-                  <hr width="100%" />
+        <div className="container-country">
+          <div className="track2">
+            <div className="container-colum">
+              <div className="div1">
+                <div className="casos">
+                  <h5>Total Cases</h5>
+                  <div className="linea">
+                    <hr width="100%" />
+                  </div>
+                  <h2>{responseData.cases}</h2>
                 </div>
-                <h2>{responseData.cases}</h2>
+                <div className="virus-v">
+                  <img
+                    className="virus"
+                    src={covidblue}
+                    alt="virus"
+                    width="140px"
+                  />
+                </div>
               </div>
-              <div className="virus-v">
-                <img
-                  className="virus"
-                  src={covidblue}
-                  alt="virus"
-                  width="140px"
-                />
-              </div>
-            </div>
 
-            <div className="div2">
-              <div className="casos">
-                <h5>Total Deaths</h5>
-                <hr />
-                <h2>{responseData.deaths}</h2>
+              <div className="div2">
+                <div className="casos">
+                  <h5>Total Deaths</h5>
+                  <hr />
+                  <h2>{responseData.deaths}</h2>
+                </div>
+                <div className="virus-v">
+                  <img
+                    className="virus"
+                    src={covidred}
+                    alt="virus"
+                    width="140px"
+                  />
+                </div>
               </div>
-              <div className="virus-v">
-                <img
-                  className="virus"
-                  src={covidred}
-                  alt="virus"
-                  width="140px"
-                />
+              <div className="div3">
+                <div className="casos">
+                  <h5>Total Recovered</h5>
+                  <hr />
+                  <h2>{responseData.recovered}</h2>
+                </div>
+                <div className="virus-v">
+                  <img
+                    className="virus"
+                    src={covidgreen}
+                    alt="virus"
+                    width="140px"
+                  />
+                </div>
               </div>
-            </div>
-            <div className="div3">
-              <div className="casos">
-                <h5>Total Recovered</h5>
-                <hr />
-                <h2>{responseData.recovered}</h2>
-              </div>
-              <div className="virus-v">
-                <img
-                  className="virus"
-                  src={covidgreen}
-                  alt="virus"
-                  width="140px"
-                />
-              </div>
-            </div>
 
-            <div className="div4">
-              <div className="casos">
-                <h5>Total Active</h5>
-                <hr />
-                <h2>{responseData.active}</h2>
+              <div className="div4">
+                <div className="casos">
+                  <h5>Total Active</h5>
+                  <hr />
+                  <h2>{responseData.active}</h2>
+                </div>
+                <div className="virus-v">
+                  <img
+                    className="virus"
+                    src={covidblue}
+                    alt="virus"
+                    width="140px"
+                  />
+                </div>
               </div>
-              <div className="virus-v">
-                <img
-                  className="virus"
-                  src={covidblue}
-                  alt="virus"
-                  width="140px"
-                />
-              </div>
-            </div>
 
-            <div className="div5">
-              <div className="casos">
-                <h5>New Cases</h5>
-                <hr />
-                <h2>{responseData.todayCases}</h2>
+              <div className="div5">
+                <div className="casos">
+                  <h5>New Cases</h5>
+                  <hr />
+                  <h2>{responseData.todayCases}</h2>
+                </div>
+                <div className="virus-v">
+                  <img
+                    className="virus"
+                    src={covidorange}
+                    alt="virus"
+                    width="140px"
+                  />
+                </div>
               </div>
-              <div className="virus-v">
-                <img
-                  className="virus"
-                  src={covidorange}
-                  alt="virus"
-                  width="140px"
-                />
-              </div>
-            </div>
 
-            <div className="div6">
-              <div className="casos">
-                <h5>New Deaths</h5>
-                <hr />
-                <h2>{0}</h2>
-              </div>
-              <div className="virus-v">
-                <img
-                  className="virus"
-                  src={covidred}
-                  alt="virus"
-                  width="140px"
-                />
+              <div className="div6">
+                <div className="casos">
+                  <h5>New Deaths</h5>
+                  <hr />
+                  <h2>{0}</h2>
+                </div>
+                <div className="virus-v">
+                  <img
+                    className="virus"
+                    src={covidred}
+                    alt="virus"
+                    width="140px"
+                  />
+                </div>
               </div>
             </div>
           </div>
+          <div>
+            <img src={geo} alt="mapamundi" className="img-geo" />
+          </div>
         </div>
       )}
-      <div>
-        <img src={geo} alt="mapamundi" className="img-geo" />
-      </div>
-    </div>
+    </>
   );
 };
 
